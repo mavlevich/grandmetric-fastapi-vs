f0d4ac69083f9986db992f78c6eda316ccb1aad5
mavlevich <evgenij.mavlevich@gmail.com>
2022-11-28 23:20:12 +0100

    after feedback on the amendments made

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from pydantic import BaseModel
 import uvicorn
 import requests
 from fastapi.testclient import TestClient
+from fastapi.encoders import jsonable_encoder
+import json
+
 
 app = FastAPI()
 
@@ -26,29 +29,66 @@ client = TestClient(app)
 
 
 # Task 2
-
-
-class Task2(BaseModel):
+# Request data model
+class DateModelForApiRequest(BaseModel):
     date: str
 
 
 @app.post("/day")
-async def day(task2: Task2):
-    # Actual date (dictionary
-    current_datetime = datetime.now()
-    day_response = requests.get("http://numbersapi.com/" + str(current_datetime.day))
-    month_response = requests.get("http://numbersapi.com/" + str(current_datetime.month))
-    year_response = requests.get("http://numbersapi.com/" + str(current_datetime.year))
+async def newday(incoming_request_with_date: DateModelForApiRequest):
+    # Zmienne dla przyszlej walidacji 
+    date_validation_day = False;
+    date_validation_month = False;
+    date_validation_year = False;
+    date_validation_length = False;
 
-    # Custom dictionary
-    # If day = month, one position is not displayed
+    # Serializacja do str
+    # serialized_dictionary_with_date = '{"date": "28.10.2022"}'                       # Przyklad danych
+    serialized_dictionary_with_date = incoming_request_with_date.json()
+
+    # Pobranie liczb dla przyszlego requestu do numbersapi
+    day = serialized_dictionary_with_date[10:12]
+    month = serialized_dictionary_with_date[13:15]
+    year = serialized_dictionary_with_date[16:20]
+
+    # Walidacja na dlugosc str, czyli rok w formacie "22" nie przejdzie 
+    if (len(serialized_dictionary_with_date) == 22):
+        date_validation_length = True
+
+    # Walidacja danych (sprawdzenie czy cyferki mamy)
+    if ((day[0] >= '0' and day[0] <= '9') and (day[1] >= '0' and day[1] <= '9')):
+        print("day OK")
+        date_validation_day = True
+    if ((month[0] >= '0' and month[0] <= '9') and (month[1] >= '0' and month[1] <= '9')):
+        print("month OK")
+        date_validation_month = True
+    if ((year[0] >= '0' and year[0] <= '9') and (year[1] >= '0' and year[1] <= '9') and 
+        (year[2] >= '0' and year[2] <= '9') and (year[3] >= '0' and year[3] <= '9')  ):
+        print("year OK")
+        date_validation_month = True
+    
+
+    # Calkowita walidacja (literki + dlugosc)
+    # Teoretycznie mozna zrobic walidacje na prawdziwa date, czyli month >= 1 and month <= 12, day >= 1 nad day <= 31
+    if (date_validation_length != True or date_validation_day != True or date_validation_month != True or date_validation_year):
+        dict_error = {
+            "error": "Incorrect request format"
+        }
+        return JSONResponse(content=dict_error)
+    
+    # Zapytanie do api 
+    day_response = requests.get("http://numbersapi.com/" + day)
+    month_response = requests.get("http://numbersapi.com/" + month)
+    year_response = requests.get("http://numbersapi.com/" + year)
+
+    # Generacja slownika dla odpowiedzi zwrotnej
     date = {
-        current_datetime.day: day_response.text,
-        current_datetime.month: month_response.text,
-        current_datetime.year: year_response.text
+        day: day_response.text,
+        month: month_response.text,
+        year: year_response.text
     }
 
-    print(date)
+    # print(date)
     return JSONResponse(content=date)
 
 
